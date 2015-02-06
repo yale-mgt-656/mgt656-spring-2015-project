9a0a3375fb4640d5f551e9a220f1084b09e32773
Burak B <burak.bekaroglu@yale.edu>
2015-02-06 03:23:55 +0000

    API Search

--- controllers/events.js
+++ controllers/events.js
@@ -121,8 +121,17 @@ function rsvp (request, response){
 }
 
   function api(request,response) {
-    var output = {events: events.all};
-    response.send(output);
+    var output = {events: []};
+    var search = request.query.search;
+    if(search){
+    for(var i=0; i<events.all.length;i++){
+     if(events.all[i].title.indexOf(search)!== -1)
+      output.events.push(events.all[i]);
+      }
+    }else{
+      output.events = events.all;
+    }
+    response.json(output);
   }
 
 /**
