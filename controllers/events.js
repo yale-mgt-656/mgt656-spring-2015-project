82eb776bdcc1ca6348a95c4d5f828761c25ceee0
smilingjaguar <jayant.gokhale@yale.edu>
2015-02-09 20:34:12 +0000

    error checks and formatting

--- controllers/events.js
+++ controllers/events.js
@@ -49,6 +49,30 @@ function newEvent(request, response){
   response.render('create-event.html', contextData);
 }
 
+
+function checkIntRange(request, fieldName, minVal, maxVal, contextData){
+  var value=null;
+  if (validator.isInt(request.body[fieldName])===false){
+    contextData.errors.push('Your' + fieldName + 'should be an integer');
+  }else{
+    value=parseInt(request.body[fieldName],10);
+    if (value>maxVal || value<minVal){
+      contextData.errors.push('Your ' + fieldName + ' should be in the range of' + minVal + '-'+ maxVal);
+    }
+  }
+  return value;
+}
+
+function checkImage(request, contextData, picture){
+  var value = picture;
+  if(value.match(/https|http/)<0){
+    contextData.errors('Your image does not begin with https or http.');
+  }else if(value.match(/\.png|\.gif/)<0){
+    contextData.errors('Your image does not end with png or gif.');
+  }
+  return value;
+}
+
 /**
  * Controller to which new events are submitted.
  * Validates the form and adds the new event to
@@ -58,10 +82,22 @@ function saveEvent(request, response){
   var contextData = {errors: []};
 
   if (validator.isLength(request.body.title, 5, 50) === false) {
-    contextData.errors.push('Your title should be between 5 and 100 letters.');
+    contextData.errors.push('Your title should be between 5 and 50 letters.');
   }
+  
+ if (validator.isLength(request.body.location, 5, 50) === false) {
+    contextData.errors.push('Your location should be between 5 and 50 letters.');
+  }
+  
+  var year = checkIntRange(request,'year',2015,2016,contextData);
+  var month = checkIntRange(request,'year',0,11,contextData);
+  var day = checkIntRange(request,'year',0,31,contextData);
+  var hour = checkIntRange(request,'year',0,23,contextData);
+  
+  var imag = checkImage(request,contextData,'image');
+  
 
-
+  
   if (contextData.errors.length === 0) {
     var newEvent = {
       title: request.body.title,
