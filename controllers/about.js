ddc19b95bb7c08f652c441f8c8c841ce9f423546
Elaine Pan <elaine.pan@yale.edu>
2015-02-11 06:45:52 +0000

    Add team logo

--- controllers/about.js
+++ controllers/about.js
@@ -4,7 +4,11 @@
  * Controller that renders our about page.
  */
 function about (request, response) {
-  var contextData = {};
+  var contextData = {
+    id: 'logo',
+    image: 'http://www.brandrepublicinsight.com/Media/Default/images/dark-logo.png'
+    
+  };
   response.render('about.html', contextData);
 }
 
