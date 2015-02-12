7b30a57edf376203f500cc46bc2d8c46f3fdef85
smilingjaguar <jayant.gokhale@yale.edu>
2015-02-12 16:44:37 +0000

    error validation and event detail page

--- controllers/events.js
+++ controllers/events.js
@@ -54,7 +54,8 @@ function checkIntRange(request, fieldName, minVal, maxVal, contextData){
   var value=null;
   if (validator.isInt(request.body[fieldName])===false){
     contextData.errors.push('Your' + fieldName + 'should be an integer');
-  }else{
+  }
+  else{
     value=parseInt(request.body[fieldName],10);
     if (value>maxVal || value<minVal){
       contextData.errors.push('Your ' + fieldName + ' should be in the range of' + minVal + '-'+ maxVal);
@@ -63,12 +64,24 @@ function checkIntRange(request, fieldName, minVal, maxVal, contextData){
   return value;
 }
 
-function checkImage(request, contextData, picture){
-  var value = picture;
-  if(value.match(/https|http/)<0){
-    contextData.errors('Your image does not begin with https or http.');
-  }else if(value.match(/\.png|\.gif/)<0){
-    contextData.errors('Your image does not end with png or gif.');
+function checkImage(request, picture, contextData){
+  var value = request.body[picture];
+  console.log('image value ' + value);
+  if(value === ''){
+    console.log('got here??');
+    contextData.errors.push('you need a image');
+  }
+  else{
+    console.log('got here?????');
+    
+    if(value.indexOf(/https|http/)<0){
+      console.log('has http ');
+      contextData.errors.push('Your image does not begin with https or http.');
+    }
+    
+    if(value.indexOf(/\.(png|gif)$/)<0){
+      contextData.errors.push('Your image does not end with png or gif.');
+    }
   }
   return value;
 }
@@ -90,13 +103,10 @@ function saveEvent(request, response){
   }
   
   var year = checkIntRange(request,'year',2015,2016,contextData);
-  var month = checkIntRange(request,'year',0,11,contextData);
-  var day = checkIntRange(request,'year',0,31,contextData);
-  var hour = checkIntRange(request,'year',0,23,contextData);
-  
-  var imag = checkImage(request,contextData,'image');
-  
-
+  var month = checkIntRange(request,'month',0,11,contextData);
+  var day = checkIntRange(request,'day',0,31,contextData);
+  var hour = checkIntRange(request,'hour',0,23,contextData);
+  var imag = checkImage(request,'image',contextData);
   
   if (contextData.errors.length === 0) {
     var newEvent = {
@@ -108,157 +118,8 @@ function saveEvent(request, response){
     };
     events.all.push(newEvent);
     response.redirect('/events');
-  }else{
-    response.render('create-event.html', contextData);
   }
-}
-
-function eventDetail (request, response) {
-  var ev = events.getById(parseInt(request.params.id));
-  if (ev === null) {
-    response.status(404).send('No such event');
-  }
-  response.render('event-detail.html', {event: ev});
-}
-
-function rsvp (request, response){
-  var ev = events.getById(parseInt(request.params.id));
-  if (ev === null) {
-    response.status(404).send('No such event');
-  }
-
-  if(validator.isEmail(request.body.email)){
-    ev.attending.push(request.body.email);
-    response.redirect('/events/' + ev.id);
-  }else{
-    var contextData = {errors: [], event: ev};
-    contextData.errors.push('Invalid email');
-    response.render('event-detail.html', contextData);    
-  }
-
-}
-
-/**
- * Export all our functions (controllers in this case, because they
- * handles requests and render responses).
- */
-module.exports = {
-  'listEvents': listEvents,
-  'eventDetail': eventDetail,
-  'newEvent': newEvent,
-  'saveEvent': saveEvent,
-  'rsvp': rsvp
-};'use strict';
-
-var events = require('../models/events');
-var validator = require('validator');
-
-// Date data that would be useful to you
-// completing the project These data are not
-// used a first.
-//
-var allowedDateInfo = {
-  months: {
-    0: 'January',
-    1: 'February',
-    2: 'March',
-    3: 'April',
-    4: 'May',
-    5: 'June',
-    6: 'July',
-    7: 'August',
-    8: 'September',
-    9: 'October',
-    10: 'November',
-    11: 'December'
-  },
-  minutes: [0, 30],
-  hours: [
-    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
-    12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23
-  ]
-};
-
-/**
- * Controller that renders a list of events in HTML.
- */
-function listEvents(request, response) {
-  var currentTime = new Date();
-  var contextData = {
-    'events': events.all,
-    'time': currentTime
-  };
-  response.render('event.html', contextData);
-}
-
-/**
- * Controller that renders a page for creating new events.
- */
-function newEvent(request, response){
-  var contextData = {};
-  response.render('create-event.html', contextData);
-}
-
-
-function checkIntRange(request, fieldName, minVal, maxVal, contextData){
-  var value=null;
-  if (validator.isInt(request.body[fieldName])===false){
-    contextData.errors.push('Your' + fieldName + 'should be an integer');
-  }else{
-    value=parseInt(request.body[fieldName],10);
-    if (value>maxVal || value<minVal){
-      contextData.errors.push('Your ' + fieldName + ' should be in the range of' + minVal + '-'+ maxVal);
-    }
-  }
-  return value;
-}
-
-function checkImage(request, contextData, picture){
-  var value = picture;
-  if(value.match(/https|http/)<0){
-    contextData.errors('Your image does not begin with https or http.');
-  }else if(value.match(/\.png|\.gif/)<0){
-    contextData.errors('Your image does not end with png or gif.');
-  }
-  return value;
-}
-
-/**
- * Controller to which new events are submitted.
- * Validates the form and adds the new event to
- * our global list of events.
- */
-function saveEvent(request, response){
-  var contextData = {errors: []};
-
-  if (validator.isLength(request.body.title, 5, 50) === false) {
-    contextData.errors.push('Your title should be between 5 and 50 letters.');
-  }
-  
- if (validator.isLength(request.body.location, 5, 50) === false) {
-    contextData.errors.push('Your location should be between 5 and 50 letters.');
-  }
-  
-  var year = checkIntRange(request,'year',2015,2016,contextData);
-  var month = checkIntRange(request,'year',0,11,contextData);
-  var day = checkIntRange(request,'year',0,31,contextData);
-  var hour = checkIntRange(request,'year',0,23,contextData);
-  
-  var imag = checkImage(request,contextData,'image');
-  
-
-  
-  if (contextData.errors.length === 0) {
-    var newEvent = {
-      title: request.body.title,
-      location: request.body.location,
-      image: request.body.image,
-      date: new Date(),
-      attending: []
-    };
-    events.all.push(newEvent);
-    response.redirect('/events');
-  }else{
+  else{
     response.render('create-event.html', contextData);
   }
 }
