f673a14884af217a823f8168309b439d1d365349
Tugce Erten <tugce.erten@yale.edu>
2015-02-13 22:07:32 +0000

    a-b testing rsvp

--- controllers/index.js
+++ controllers/index.js
@@ -14,6 +14,17 @@ function randomWord(arr) {
 }
 
 function index (request, response) {
+  var exp = 0.5;
+  var r = Math.random();
+  if (r<=exp) {
+    response.redirect('/original');
+  }
+  else {
+    response.redirect('/experiment');
+  }
+}
+
+function experiment (request, response) {
   var now = new Date();
   var contextData = {
     'title': 'MGT656',
@@ -27,9 +38,28 @@ function index (request, response) {
       contextData.events.push(event);
     }
   }
-  response.render('index.html', contextData);
+  response.render('index_rsvp.html', contextData);
+}
+
+function original (req, res) {
+  var now = new Date();
+  var contextData = {
+    'title': 'MGT656',
+    'tagline': 'The world\'s best Eventbrite clone',
+    'events': [],
+    'tagword': randomWord(words),
+  };
+  for (var i=0; i < events.all.length; i++){
+    var event = events.all[i];
+    if(event.date > now) {
+      contextData.events.push(event);
+    }
+  }
+  res.render('layout.html', contextData);
 }
 
 module.exports = {
-  index: index
+  'index': index,
+  'original': original,
+  'experiment': experiment
 };
