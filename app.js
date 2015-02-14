9c05d268d23617898889141a33f18a097f830965
haarvs <harvey.yang@outlook.com>
2015-02-14 17:36:50 +0000

    added event details page, fixed create event issues

--- app.js
+++ app.js
@@ -23,5 +23,7 @@ app.get('/events', eventControllers.listEvents);
 app.get('/events/new', eventControllers.newEvent);
 app.get('/api/events', eventControllers.api);
 app.post('/events/new', eventControllers.saveEvent);
+app.get('/events/:id', eventControllers.eventDetail);
+app.post('/events/:id', eventControllers.rsvp);
 
 module.exports = app;
