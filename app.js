5090a050b75d0cada9ef3510cad435e03820dfef
cameronrout <cameron.rout@yale.edu>
2015-02-18 05:23:45 +0000

    added rout for /pay as well as /events/pay

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ configure(app);
 // Add routes mapping URLs to controllers
 app.get('/', indexControllers.index);
 app.get('/about', aboutControllers.about);
+app.get('/pay', eventControllers.pay);
 app.get('/events', eventControllers.listEvents);
 app.get('/events/new', eventControllers.newEvent);
 app.get('/events/pay', eventControllers.pay);
