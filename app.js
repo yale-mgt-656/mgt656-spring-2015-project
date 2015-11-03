812d5831c9e9097c59b898803364d98bf6f7f6c0
mezyad <mezyad.almasoud@yale.edu>
2015-11-03 19:58:12 +0000

    Added a route for API search option

--- app.js
+++ app.js
@@ -22,5 +22,6 @@ app.get('/about', aboutControllers.about);
 app.get('/events', eventControllers.listEvents);
 app.get('/events/new', eventControllers.newEvent);
 app.post('/events/new', eventControllers.saveEvent);
+app.post('/api/events', eventControllers.saveEvent);
 
 module.exports = app;
