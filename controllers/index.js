5703b3f73e5eb81e805c0fdc8a02524937d33a48
Shadin Cornelio <shacorne23@gmail.com>
2015-11-04 19:48:10 +0000

    Changes to index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,13 +1,16 @@
 'use strict';
-
+var events = require('../models/events');
 /**
  * Controller that renders our index (home) page.
  */
 function index (request, response) {
   var contextData = {
     'title': 'RSVPd',
-    'tagline': 'The one stop for all events in town!'
+    'tagline': 'The one stop for all events in town!',
+    'events': events.all,
+    'time': currentTime
   };
+  var currentTime = new Date();
   response.render('index.html', contextData);
 }
 
