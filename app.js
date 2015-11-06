b905ddfd7e266eba295839a3932aa82ce8eb4f8d
Jeremy Liu <jeremy.liu@yale.edu>
2015-11-06 17:58:05 +0000

    Refactored API into it's own controller

--- app.js
+++ app.js
@@ -8,9 +8,7 @@ var configure = require('./config.js');
 var indexControllers = require('./controllers/index.js');
 var aboutControllers = require('./controllers/about.js');
 var eventControllers = require('./controllers/events.js');
-
-// Import our models
-var Event     = require('./models/events');
+var apiControllers = require('./controllers/api.js');
 
 // Create our express app
 var app = express();
@@ -18,43 +16,15 @@ var app = express();
 // Configure it
 configure(app);
 
-// ROUTES FOR OUR API
-// =============================================================================
-var router = express.Router();              // get an instance of the express Router
-
-// middleware to use for all requests
-router.use(function(req, res, next) {
-    // do logging
-    console.log('Something is happening.');
-    next(); // make sure we go to the next routes and don't stop here
-});
-
-// on routes that end in /bears
-// ----------------------------------------------------
-router.route('/events')
-
-    // get all the events (accessed at GET http://localhost:8080/api/events)
-
-    .get(function(req, res) {
-        res.json(Event.all)
-    });
-
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });
-});
-
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
-app.use('/api', router);
-
-
 // Add routes mapping URLs to controllers
 app.get('/', indexControllers.index);
 app.get('/about', aboutControllers.about);
 app.get('/events', eventControllers.listEvents);
 app.get('/events/:id', eventControllers.eventDetail);
 app.get('/events/new', eventControllers.newEvent);
+app.get('/api', apiControllers.viewApi);
+app.get('/api/events', apiControllers.listEventsJSON);
+// app.get('/api/events?search=')
 app.post('/events/new', eventControllers.saveEvent);
 
 app.get('/sprints', function (req, res) {
