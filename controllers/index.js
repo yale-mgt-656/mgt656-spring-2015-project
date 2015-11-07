563c24be26a85910d6f373482c9652ea2d7583d8
Greg <wgmuecke@gmail.com>
2015-11-07 20:10:10 +0000

    add bootstrap and events on index

--- controllers/index.js
+++ controllers/index.js
@@ -3,10 +3,15 @@
 /**
  * Controller that renders our index (home) page.
  */
+var events = require('../models/events');
+
 function index (request, response) {
+  var currentTime = new Date();
   var contextData = {
     'title': 'MGT 656',
-    'tagline': 'This is a tagline.'
+    'tagline': 'This is a tagline.',
+    'events': events.all,
+    'time': currentTime
   };
   response.render('index.html', contextData);
 }
