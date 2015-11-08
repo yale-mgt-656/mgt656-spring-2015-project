43ab8828720805e8dc2356673e8a86904de70f8e
jsbutler151 <john.s.butler@yale.edu>
2015-11-08 20:29:22 +0000

    Added reports pages

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var configure = require('./config.js');
 var indexControllers = require('./controllers/index.js');
 var aboutControllers = require('./controllers/about.js');
 var eventControllers = require('./controllers/events.js');
+var reportsControllers = require('./controllers/reports.js');
 
 
 // Create our express app
@@ -19,6 +20,13 @@ configure(app);
 // Add routes mapping URLs to controllers
 app.get('/', indexControllers.index);
 app.get('/about', aboutControllers.about);
+app.get('/reports', reportsControllers.reports);
+app.get('/reports/1', reportsControllers.scrum1);
+app.get('/reports/2', reportsControllers.scrum2);
+app.get('/reports/3', reportsControllers.scrum3);
+app.get('/reports/4', reportsControllers.scrum4);
+app.get('/reports/5', reportsControllers.scrum5);
+app.get('/reports/6', reportsControllers.scrum6);
 app.get('/events', eventControllers.listEvents);
 app.get('/events/new', eventControllers.newEvent);
 app.post('/events/new', eventControllers.saveEvent);
