3c7595438edf0853c51f6ec7feee513beb08db77
peapumpkin <lea.lupkin@yale.edu>
2015-11-12 00:54:16 +0000

    added create event form validation

--- controllers/events.js
+++ controllers/events.js
@@ -49,6 +49,20 @@ function newEvent(request, response){
   response.render('create-event.html', contextData);
 }
 
+function checkIntRange(request, fieldName, minVal, maxVal, contextData){
+  var value = null;
+  if (validator.isInt(request.body[fieldName]) === false) {
+    contextData.errors.push('Your ' + fieldName +' should be an integer.');
+}
+else {
+  value = parseInt(request.body[fieldName], 10);
+    if (value > maxVal || value < minVal) {
+    contextData.errors.push('Your ' + fieldName + ' should be in the range ' + minVal + '-' + maxVal);
+  }
+}
+return value;
+  }
+
 /**
  * Controller to which new events are submitted.
  * Validates the form and adds the new event to
@@ -57,11 +71,28 @@ function newEvent(request, response){
 function saveEvent(request, response){
   var contextData = {errors: []};
 
-  if (validator.isLength(request.body.title, 5, 50) === false) {
-    contextData.errors.push('Your title should be between 5 and 100 letters.');
+  if (validator.isLength(request.body.title, 3, 50) === false) {
+    contextData.errors.push('Your title should be between 3 and 100 letters.');
+  }
+  
+  if (validator.isLength(request.body.location, 3, 50) === false) {
+    contextData.errors.push('Your location should be between 3 and 50 letters.');
+  }
+    
+  var year = checkIntRange(request, 'year', 2015, 2016, contextData);
+  var month = checkIntRange(request, 'month', 0, 11, contextData);
+  var day = checkIntRange(request, 'day', 1, 31, contextData);
+  var hour = checkIntRange(request, 'hour', 0, 23, contextData);
+  
+ 
+  if (validator.isURL(request.body.image) === false) {
+    contextData.errors.push('Your image must be a URL');
   }
 
-
+  if (validator.matches(request.body.image, /\.(png|gif)$/) === false) {
+    contextData.errors.push('Your image must be a .png or .gif');
+  }
+  
   if (contextData.errors.length === 0) {
     var newEvent = {
       title: request.body.title,
