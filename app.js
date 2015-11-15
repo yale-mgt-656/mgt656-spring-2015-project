39c72841680177dfe468291bfd6d0a8e62ea9878
zhaffner <zeev.haffner@yale.edu>
2015-11-15 22:36:57 +0000

    Zeev's basic changes

--- app.js
+++ app.js
@@ -22,7 +22,9 @@ app.get('/about', aboutControllers.about);
 app.get('/events', eventControllers.listEvents);
 app.get('/api/events', eventControllers.api);
 app.get('/events/new', eventControllers.newEvent);
-app.get('/events/:id', eventControllers.eventDetail); 
 app.post('/events/new', eventControllers.saveEvent);
+app.get('/events/:id([0-9]+)', eventControllers.eventDetail); 
+app.post('/events/:id([0-9]+)', eventControllers.rsvp); 
+
 
 module.exports = app;
