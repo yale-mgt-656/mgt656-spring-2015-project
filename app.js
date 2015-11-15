6f115d0800ceece39e3114d35a2e1c91301e3882
glamorama <trenttolley@gmail.com>
2015-11-15 19:31:28 +0000

    Trent added API search funtionality

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.get('/about', aboutControllers.about);
 app.get('/events', eventControllers.listEvents);
 app.get('/events/new', eventControllers.newEvent);
 app.get('/events/:id' , eventControllers.eventDetail);
+app.get('/api/events' , eventControllers.api);
 app.post('/events/new', eventControllers.saveEvent);
 app.post('/events/:id', eventControllers.rsvp);
 
