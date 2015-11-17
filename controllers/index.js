20121b8702cfa7c0eafad011378ee3b92024ec37
stakao25 <shaun.takao@yale.edu>
2015-11-17 04:45:45 +0000

    added event list to homepage

--- controllers/index.js
+++ controllers/index.js
@@ -3,11 +3,20 @@
 /**
  * Controller that renders our index (home) page.
  */
+var events = require('../models/events');
 function index (request, response) {
+  var now = new Date();
   var contextData = {
-    'title': 'MGT 656',
-    'tagline': 'You are doomed (just kidding).'
+    'title': "Fragrant Ninja's Dojo of Doom",
+    'tagline': "Yes, this is what a dojo smells like",
+    'events': []
   };
+  for(var i=0; i < events.all.length; i++){
+    var event = events.all[i];
+    if(event.date > now){
+      contextData.events.push(event);
+    }
+  }
   response.render('index.html', contextData);
 }
 
