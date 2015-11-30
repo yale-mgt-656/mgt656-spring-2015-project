731d76b0d29383b3385b9077a4da7a3f1302ab9e
fsarti <fsarti@gmail.com>
2015-11-30 18:17:42 +0000

    Revert "Add API and API search"
    This reverts commit ef8c5305a1502bc76144eff7739bdebe5003164c.

--- app.js
+++ app.js
@@ -21,7 +21,6 @@ app.get('/', indexControllers.index);
 app.get('/about', aboutControllers.about);
 app.get('/events', eventControllers.listEvents);
 app.get('/events/new', eventControllers.newEvent);
-app.get('/api/events/', eventControllers.api);
 app.post('/events/new', eventControllers.saveEvent);
 
 module.exports = app;
