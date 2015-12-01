a4ca4233a846d883b88ec8b985231721cbdf8e0c
jeffmcq1616 <jeff.mchugh@yale.edu>
2015-12-01 16:13:36 +0000

    created pages for sprint6 report and final report

--- controllers/sprint-report4.js
+++ controllers/sprint-report4.js
@@ -6,14 +6,26 @@
 function sprintreport (request, response) {
   var contextData = {};
   response.render('sprint-report4.html', contextData);
-}
+};
 
 function sprintreport5 (request, response) {
   var contextData = {};
   response.render('sprint-report5.html', contextData);
-}
+};
+
+function sprintreport6 (request, response) {
+  var contextData = {};
+  response.render('sprint-report6.html', contextData);
+};
+
+function finalreport (request, response) {
+  var contextData = {};
+  response.render('final-report.html', contextData);
+};
 
 module.exports = {
   sprintreport: sprintreport,
-  sprintreport5: sprintreport5
+  sprintreport5: sprintreport5,
+  sprintreport6: sprintreport6,
+  finalreport: finalreport
 };
