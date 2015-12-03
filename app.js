a3bfd94ad3465c8a4602ed1bd6f2ac77665543d4
ricardosota <ricardo.sota@yale.edu>
2015-12-03 04:33:37 +0000

    added again the app.get for redirect new events

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.get('/about', aboutControllers.about);
 app.get('/events', eventControllers.listEvents);
 app.get('/events/new', eventControllers.newEvent);
 app.post('/events/new', eventControllers.saveEvent);
+app.get('/events/:id', eventControllers.eventDetail);
 app.get('/api/events', eventControllers.api);
 
 
