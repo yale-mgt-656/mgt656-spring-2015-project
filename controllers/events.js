1d6455e340dbce4bd04d2cb88dbedf4170ffcf7e
jeg228 <jessica.gallegos@yale.edu>
2015-12-05 22:37:14 +0000

    fix to new event integer tests

--- controllers/events.js
+++ controllers/events.js
@@ -49,6 +49,19 @@ function newEvent(request, response){
   response.render('create-event.html', contextData);
 }
 
+function checkIntRange(request, fieldName, minVal, maxVal, contextData){
+  var value = null;
+  if (validator.isInt(request.body[fieldName]) == false) {
+    contextData.errors.push('Your' + fieldName + 'should be an integer');
+  }else{
+    value = parseInt (request.body[fieldName], 10);
+    if (value > maxVal || value < minVal) {
+    contextData.errors.push('Your' + [fieldName] + 'should be between' + minVal + 'and' + maxVal);
+    }
+  }
+  return value;
+}
+
 /**
  * Controller to which new events are submitted.
  * Validates the form and adds the new event to
@@ -60,8 +73,12 @@ function saveEvent(request, response){
   if (validator.isLength(request.body.title, 5, 50) === false) {
     contextData.errors.push('Your title should be between 5 and 100 letters.');
   }
-
-
+  
+  var year = checkIntRange(request, 'year', 2015, 2016, contextData);
+  var month = checkIntRange(request, 'month', 0, 11, contextData);  
+  var day = checkIntRange(request, 'day', 1, 31, contextData);
+  var hour = checkIntRange(request, 'hour', 0, 23, contextData);
+  
   if (contextData.errors.length === 0) {
     var newEvent = {
       title: request.body.title,
