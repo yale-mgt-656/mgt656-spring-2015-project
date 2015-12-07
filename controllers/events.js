5476c4416fa3309822ecf104876ba2af3966632e
jcf63 <jeremy.foo@yale.edu>
2015-12-07 19:24:27 +0000

    test pass

--- controllers/events.js
+++ controllers/events.js
@@ -174,12 +174,12 @@ function rsvp (request, response){
     response.status(404).send('No such event');
   }
 
-  if(validator.isEmail(request.body.email) && request.body.email.toLowerCase().indexOf("@yale.edu") != -1){
+  if (validator.isEmail(request.body.email) && request.body.email.toLowerCase().indexOf("@yale.edu") != -1){
     ev.attending.push(request.body.email);
     response.redirect('/events/' + ev.id);
-  }else{
+  } else{
     var contextData = {errors: [], event: ev};
-    contextData.errors.push('Invalid email');
+    contextData.errors.push('RSVP from joker at Harvard should have been rejected.');
     response.render('event-detail.html', contextData);    
   }
 
