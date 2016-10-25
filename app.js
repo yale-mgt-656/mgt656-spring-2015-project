3b637983455d73d238753bd7ad0587cdcfca31b9
Stephanie Hickman <stephanie.hickman@yale.edu>
2016-10-25 13:48:04 +0000

    add documentation and add stylesheet

--- app.js
+++ app.js
@@ -18,8 +18,10 @@ configure(app);
 // Add routes mapping URLs to controllers
 app.get('/', indexControllers.index);
 app.get('/about', aboutControllers.about);
+app.get('/docreport1', aboutControllers.documentation1);
 app.get('/events', eventControllers.listEvents);
 app.get('/events/new', eventControllers.newEvent);
 app.get('/api/events', eventControllers.api);
 app.post('/events/new', eventControllers.saveEvent);
+
 module.exports = app;
