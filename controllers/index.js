ca3ccd6d75e8035de9dc55dfa14586d0c6efc005
Svitozar <svitozar.omelko@yale.edu>
2016-10-28 18:50:08 +0000

    List of Events with Links on Home Page

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,13 @@
 'use strict';
-
+var events = require('../models/events');
 /**
  * Controller that renders our index (home) page.
  */
 function index (request, response) {
   var contextData = {
-    'title': 'MGT 656',
-    'tagline': 'You are doomed (just kidding).'
+    'title': 'Our Eventbrite Ripoff',
+    'tagline': 'Reckless Sound is Awesome!',
+    'events': events.all
   };
   response.render('index.html', contextData);
 }
