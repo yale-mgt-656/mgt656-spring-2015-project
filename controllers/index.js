64733177368c54d90fb70aef12d003c9d26078e4
Svitozar <svitozar.omelko@yale.edu>
2016-10-29 12:01:49 +0000

    Hide Events Complete

--- controllers/index.js
+++ controllers/index.js
@@ -4,11 +4,19 @@ var events = require('../models/events');
  * Controller that renders our index (home) page.
  */
 function index (request, response) {
+  var now = new Date();
   var contextData = {
     'title': 'Our Eventbrite Ripoff',
     'tagline': 'Reckless Sound is Awesome!',
-    'events': events.all
+    'events': []
   };
+  for(var i=0; i < events.all.length; i++){
+    var event = events.all[i];
+    if(event.date > now){
+      contextData.events.push(event);
+    }
+  }
+ 
   response.render('index.html', contextData);
 }
 
