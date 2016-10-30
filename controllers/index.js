1afa403ccba3bda935b4913b2ac24b04b2313bb1
Div <divesh.kumar@yale.edu>
2016-10-30 16:42:34 +0000

    adding logo

--- controllers/index.js
+++ controllers/index.js
@@ -5,7 +5,7 @@
  */
 function index (request, response) {
   var contextData = {
-    'title': 'MGT 656',
+    'title': 'MGT 656 Misty Meadow',
     'tagline': 'You are doomed (just kidding).'
   };
   response.render('index.html', contextData);
