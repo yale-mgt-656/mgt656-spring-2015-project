f3b888ea24092a1831e7bdf441a9026655fed25f
Div <divesh.kumar@yale.edu>
2016-10-30 18:10:00 +0000

    added events on home page

--- controllers/index.js
+++ controllers/index.js
@@ -8,8 +8,9 @@ var events = require('../models/events');
 function index (request, response) {
   var contextData = {
     'title': 'MGT 656 Misty Meadow',
-    'tagline': 'You are doomed (just kidding).'
-    'events' : events.all,
+    'tagline': 'You are doomed (just kidding).',
+    'events': events.all
+    
   };
   response.render('index.html', contextData);
 }
