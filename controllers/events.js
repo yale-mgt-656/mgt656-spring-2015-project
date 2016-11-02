50db71227b781bb3f70d99d03cf1dc7c24f2bd32
lixing liang <leolianglx@gmail.com>
2016-11-02 01:49:06 +0000

    minor changes

--- controllers/events.js
+++ controllers/events.js
@@ -71,7 +71,7 @@ function saveEvent(request, response){
       attending: []
     };
     events.all.push(newEvent);
-    response.redirect('/events');
+    response.redirect('/events'+events.all.length);
   }else{
     response.render('create-event.html', contextData);
   }
