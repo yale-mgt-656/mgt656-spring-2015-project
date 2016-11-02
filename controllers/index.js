b70fc3ff1615dd44fd0a2a636d3ec9f05d31a1ce
Leah007 <leah.karlins@yale.edu>
2016-11-02 02:52:16 +0000

    Hid past events on homepage. Also separated RSVP code from Events list on events-list.html

--- controllers/index.js
+++ controllers/index.js
@@ -7,9 +7,18 @@ var events = require('../models/events');
  * Controller that renders our index (home) page.
  */
 function index (request, response) {
+  var now = new Date(); 
   var contextData = {
-    'events': events.all,
+    'events': []
   };
+  for(var i=0; i < events.all.length; i++){
+    var event = events.all[i];
+    if(event.date > now){
+      contextData.events.push(event);
+    }
+  }
+  
+ // contextData.events = events.all;
   response.render('index.html', contextData);
 }
 
