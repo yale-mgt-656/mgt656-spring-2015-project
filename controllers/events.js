9ff0f68a3579c266dcb0cc89de2e4269169c2990
lixing liang <leolianglx@gmail.com>
2016-11-03 01:20:04 +0000

    validation

--- controllers/events.js
+++ controllers/events.js
@@ -73,6 +73,13 @@ function saveEvent(request, response){
   if (validator.isLength(request.body.title, 5, 50) === false) {
     contextData.errors.push('Your title should be between 5 and 100 letters.');
   }
+  
+  var year = checkIntRange(request, 'year', 2015, 2016, contextData);
+  var month = checkIntRange(request, 'month', 0, 11, contextData);
+  var day = checkIntRange(request, 'day', 0, 31, contextData);
+  var hour = checkIntRange(request, 'hour', 0, 23, contextData);
+  
+  
   if (validator.isInt(request.body.year) === false) {
     contextData.errors.push('Your year should be an integer.');
   }
