ba0b1ae2fd5db9a256ea2347d59b5a732664e605
Div <divesh.kumar@yale.edu>
2016-11-04 06:36:11 +0000

    39 tests passed

--- controllers/events.js
+++ controllers/events.js
@@ -111,7 +111,7 @@ var hour = checkIntRange(request, 'hour', 0, 23, contextData);
       attending: []
     };
     events.all.push(newEvent);
-    response.redirect('/events');
+    response.redirect('/events/:id([0-9]+)');
     
   }else{
     response.render('create-event.html', contextData);
