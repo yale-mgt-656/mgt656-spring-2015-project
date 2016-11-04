1dc8d760fc526f1cc094f579c19066ddd8f77ee8
Div <divesh.kumar@yale.edu>
2016-11-04 03:20:10 +0000

    api

--- app.js
+++ app.js
@@ -23,10 +23,10 @@ app.get('/about', aboutControllers.about);
 app.get('/events', eventControllers.listEvents);
 app.get('/events/new', eventControllers.newEvent);
 app.post('/events/new', eventControllers.saveEvent);
-
+app.get('api/events', eventControllers.api);
 app.get('/events/:id([0-9]+)', eventControllers.eventDetail);
 app.post('/events/:id([0-9]+)', eventControllers.rsvp);
-//app.post('/events/.id', eventControllers.saveEvent.id);
+
 
 
 
--- controllers/events.js
+++ controllers/events.js
@@ -111,7 +111,8 @@ var hour = checkIntRange(request, 'hour', 0, 23, contextData);
       attending: []
     };
     events.all.push(newEvent);
-    response.redirect('/events');
+    response.redirect('/events/:id([0-9]+)');
+    
   }else{
     response.render('create-event.html', contextData);
   }
@@ -140,6 +141,11 @@ function rsvp (request, response){
     response.render('event-detail.html', contextData);    
   }
 
+}
+
+function api(request, response){
+  var output = {events: events.all};
+  response.json(output);
 }
 
 /**
