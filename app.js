30cf760254cc7e422fb219dd90ba102d72fa6b95
Mario Thomas <mario.thomas@yale.edu>
2016-11-07 14:13:22 +0000

    added event details page, updated footer

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ configure(app);
 app.get('/', indexControllers.index);
 app.get('/about', aboutControllers.about);
 app.get('/events', eventControllers.listEvents);
+app.get('/events/:id', eventControllers.eventDetail);
 app.get('/events/new', eventControllers.newEvent);
 app.post('/events/new', eventControllers.saveEvent);
 
