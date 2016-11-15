0330e1e6635eb64e10f93dc55b3527442dc90c9a
Nate Kaufman <nate.kaufman@yale.edu>
2016-11-15 21:32:34 +0000

    modify app.js

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.get('/', indexControllers.index);
 app.get('/about', aboutControllers.about);
 app.get('/events', eventControllers.listEvents);
 app.get('/events/new', eventControllers.newEvent);
+app.get('/events/:id([0-9]+)', eventControllers.eventDetail);
  
 app.post('/events/new', eventControllers.saveEvent);
 
