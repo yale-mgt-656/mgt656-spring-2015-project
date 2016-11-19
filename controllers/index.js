0f773e26b8659897c91a07965f02bb48eb3b5d61
Div <divesh.kumar@yale.edu>
2016-11-19 00:14:55 +0000

    navigation bar

--- controllers/index.js
+++ controllers/index.js
@@ -8,9 +8,11 @@ var events = require('../models/events');
 function index (request, response) {
   var now = new Date();
   var contextData = {
-    'title': 'MGT 656 Misty Meadow',
-    'tagline': 'You are doomed (just kidding).',
+    'title': 'Misty Meadow\'s',
+    'tagline': 'You can browse and register for upcoming events.',
     'events': []
+      
+      //'events': events.all
     
   };
   for(var i=0; i < events.all.length; i++){
